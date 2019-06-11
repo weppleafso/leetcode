8cd56b974b6b1cdbe7cce8df0a3598802d4191f4
wuzhipeng <weppleafso@163.com>
2019-06-11 18:21:47 +0800

    ++

--- answer/1043.js
+++ answer/1043.js
@@ -4,14 +4,42 @@
  * @return {number}
  */
 var maxList = [];
+var P = [];
 var maxSumAfterPartitioning = function (A, K) {
-    for(let i = 0;i<K;i++){
-        maxList[i] = [];
-        for(let i = 0,len = A.length;i<len;i++){
-            
+    maxList.length = 0;
+    P.length = 0;
+    for (let i = 0, len = A.length; i < len; i++) {
+        maxList.push([]);
+        maxList[i][0] = A[i];
+        for (let j = 1; j < K; j++) {
+            if (i + j < len) {
+                maxList[i][j] = Math.max(A[i + j], maxList[i][j - 1]);
+            }
         }
     }
+    console.log(maxList);
+    for(let i = 0;i<K;i++){
+        P[i] = maxList[0][i] * (i + 1);
+    }
+    return cal(A, K, A.length);
 };
-var cal = function(A,K){
+var cal = function (A, K, n) {
+    if (P[n - 1] != null) {
+        return P[n-1];
+    }
     
+    let sum = 0;
+    for (let i = 1; i <= K; i++) {
+        if (n > i) {
+            let pre = maxList[n - i][i - 1] * i + cal(A, K, n - i);
+            console.log(pre);
+            sum = Math.max(sum, pre);
+        }
+
+    }
+    P[n - 1] = sum;
+    // console.log(sum);
+    // P[n] = sum;
+    return sum;
 }
+let ret = maxSumAfterPartitioning([1,15,7,9,2,5,10], 3);
